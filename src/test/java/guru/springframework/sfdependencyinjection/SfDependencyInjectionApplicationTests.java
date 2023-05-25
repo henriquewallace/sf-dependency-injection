3f512c4d5d95e919f8cc32fe22289868b8fda08f
henriquewallace <henrique.wallace@hotmail.com>
2023-05-25 00:20:52 -0300

    initialize project and add readme

---
+++ src/test/java/guru/springframework/sfdependencyinjection/SfDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfdependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfDependencyInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
