e6f2101819f7af664e7d1721a021526c0f2bc052
henriquewallace <henrique.wallace@hotmail.com>
2023-05-30 23:56:40 -0300

    Add profile default

--- src/main/java/guru/springframework/sfdependencyinjection/services/I18nPortugueseGreetingService.java
+++ src/main/java/guru/springframework/sfdependencyinjection/services/I18nPortugueseGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.sfdependencyinjection.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("PT")
+@Profile({"PT", "default"})
 @Service("i18nService")
 public class I18nPortugueseGreetingService implements GreetingService {
     @Override
