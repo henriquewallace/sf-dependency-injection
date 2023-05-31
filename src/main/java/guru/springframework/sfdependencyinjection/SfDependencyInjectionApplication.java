d00b89e117ea3004c26d313f7517cb4173bf4f72
henriquewallace <henrique.wallace@hotmail.com>
2023-05-30 23:09:16 -0300

    use primary bean

--- src/main/java/guru/springframework/sfdependencyinjection/SfDependencyInjectionApplication.java
+++ src/main/java/guru/springframework/sfdependencyinjection/SfDependencyInjectionApplication.java
@@ -16,9 +16,8 @@ public class SfDependencyInjectionApplication {
 
         MyController myController = (MyController) context.getBean("myController");
 
-        String greeting = myController.sayHello();
-
-        System.out.println(greeting);
+        System.out.println("----- Primary Bean");
+        System.out.println(myController.sayHello());
 
         System.out.println("----- Property");
 
--- src/main/java/guru/springframework/sfdependencyinjection/controllers/MyController.java
+++ src/main/java/guru/springframework/sfdependencyinjection/controllers/MyController.java
@@ -1,13 +1,19 @@
 package guru.springframework.sfdependencyinjection.controllers;
 
+import guru.springframework.sfdependencyinjection.services.PrimaryGreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private final PrimaryGreetingService primaryGreetingService;
+
+    public MyController(PrimaryGreetingService primaryGreetingService) {
+        this.primaryGreetingService = primaryGreetingService;
+    }
 
     public String sayHello() {
         System.out.println("Hello World!");
 
-        return "Hi folks!";
+        return primaryGreetingService.sayGreeting();
     }
 }
